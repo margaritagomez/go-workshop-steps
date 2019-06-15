111fc8936972260024ee081696f9b6ff49485f93
Margarita Gómez <mgomez@truora.com>
2019-06-15 01:03:34 -0500

    Introduce mux router

--- api/main.go
+++ api/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", printPath)
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter()
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func printPath(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", r.URL.Path)
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
